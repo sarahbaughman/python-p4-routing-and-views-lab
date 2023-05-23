ce11f0d1ee81151897b73d894c98cd1b18071117
Sarah Baughman <sarahbaughman92@gmail.com>
2023-05-23 19:40:26 -0400

    finished lab

--- server/app.py
+++ server/app.py
@@ -4,5 +4,39 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    # print (f'{parameter}')
+    print(parameter)
+    # return f'{parameter}'
+    return parameter
+
+@app.route('/count/<int:number>')
+def count(number):
+    count = f''
+    for n in range(number):
+        count += f'{n}\n' 
+    #     # /n is what creates the line break, if  you swap it out with anything else,
+    #     # even another number it doesn't work, stays in one line 
+    return count
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return str(num1 + num2)
+    elif operation == '-':
+        return str(num1 - num2)
+    elif operation == '*':
+        return str(num1 * num2)
+    elif operation == '/':
+        return str(num1 / num2)
+    else:
+        return "Looks like you didn't use the right operator in your URL."
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
